d9d3cfb21e499a3cb7372dafd27b1161be7f0132
sinumerik <2603049@gmail.com>
2020-06-04 17:34:04 +0300

    added variables in main.js and addedd alert and console.log function

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-let 
+let money,
+    income,
+    addExpenses,
+    deposit,
+    mission,
+    period;
+
+alert('Hi there, press "OK" for close this window');
+
+console.log('This string in console');
