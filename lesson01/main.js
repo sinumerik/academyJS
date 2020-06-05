2f3c9aae239deaadabb633949a863bffdc83bcb9
sinumerik <2603049@gmail.com>
2020-06-05 12:30:14 +0300

    added values for variables

--- lesson01/main.js
+++ lesson01/main.js
@@ -1,9 +1,9 @@
-let money,
-    income,
-    addExpenses,
-    deposit,
-    mission,
-    period;
+let money = 30000,
+    income = 'freelance',
+    addExpenses = 'credit, utulityBills, mobile, internet',
+    deposit = false,
+    mission = 120000,
+    period = 12;
 
 alert('Hi there, press "OK" for close this window');
 
