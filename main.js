8f975697cd97f8da02a6a9337851fea4a69b2ff1
sinumerik <2603049@gmail.com>
2020-06-10 18:18:13 +0300

    complete hard lesson04

---
+++ main.js
@@ -0,0 +1,19 @@
+'use strict';
+
+let arr = ['456135', '546843', '236236', '8965823', '346347', '431287', '3468'];
+
+for(let i = 0; i < arr.length; i++) {
+    if (arr[i][0] === '2' || arr[i][0] === '4') {
+        console.log(arr[i]);
+    }
+}
+
+nextPrime:
+for(let i = 2; i <= 100; i++) {
+    for(let j = 2; j < i; j++) {
+        if( i % j === 0 ) {
+            continue nextPrime;
+        }
+    }
+    console.log(i);
+}
