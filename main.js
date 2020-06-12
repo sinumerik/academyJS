6bb4397f6e01ab4bdbdf7531e862b5bd9a658c62
sinumerik <2603049@gmail.com>
2020-06-12 17:11:18 +0300

    complete lesson07

--- main.js
+++ main.js
@@ -2,103 +2,116 @@
 
 // для проверки на число
 let isNumber = function(num) {
-    return ( isNaN(parseFloat(num)) );
+    if ( isNaN(num) === true || num === null || num === 0 ) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 let money,
-    income = 'freelance',
-    addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую', ''),
-    deposit = confirm('Есть ли у вас депозит в банке?'), // true/false
-    mission = 120000,
-    period = 12,
-    expenses = [];
-
-let start = function() {
-    do {
-        money = prompt('Ваш месячный доход?', '');
-    } while( isNumber(money) );
+    start = function() {
+        do {
+            money = +prompt('Ваш месячный доход?', '');
+        } while( isNumber(money) );
 };
 
 start();
 
-// сумма обязательных расходов за месяц
-let getExpensesMonth = () => {
+let appData = {
+    // дополнительные доходы
+    income: {},
+    addIncome: [],
+    // обязательные расходы
+    expenses: {},
+    // возможные расходы
+    addExpenses: [],
+    deposit: false, // депозит в банке
+    mission: 60000, // цель, сумма, которую хотим собрать 
+    period: 12, // рассчетный период сбора цели
+    budget: money,  // месячный заработок
+    budgetDay: 0, // дневной бюджет
+    budgetMonth: 0,
+    expensesMonth: 0,
 
-    let sum = 0;
+    // спрашиваем пользователя о возможных расходах и наличии депозита
+    asking: function() {
+        appData.addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую', '');
+        appData.addExpenses = appData.addExpenses.toLowerCase().split(',');
+        appData.deposit = confirm('Есть ли у вас депозит в банке?'); // true/false
 
-    for(let i = 0; i < 2; i++) {
+        for(let i = 0; i < 2; i++) {
 
-        expenses[i] = prompt('Введите обязательную статью расходов?', '');
+            // переменная для хранения ответа пользователя
+            let ask = prompt('Введите обязательную статью расходов?', '');
 
-        // временная переменная, для проверки на число
-        let temp = prompt('Во сколько это обойдется?', '');
+            // временная переменная, для проверки на число
+            let temp = +prompt('Во сколько это обойдется?', '');
 
-        while( isNumber(temp) ) {
-            temp = prompt('Во сколько это обойдется?', '');
+            while( isNumber(temp) ) {
+                temp = +prompt('Во сколько это обойдется?', '');
+            }
+
+            // создаем "ключ: значение" из ответов пользователя
+            appData.expenses[ask] = temp;
+        }
+    },
+
+    // сумма обязательных расходов за месяц
+    getExpensesMonth: function() {
+        let sum = 0;
+
+        for (let key in appData.expenses) {
+            sum += +appData.expenses[key];
         }
 
-        sum += +temp;
-    }
-
-    return sum;
-};
-
-let expensesAmount = getExpensesMonth();
-
-// месячный бюджет с учетом обязательных расходов
-let getAccumulatedMonth = budget => {
-    return ( +budget - expensesAmount);
-};
-
-let accumulatedMonth = getAccumulatedMonth(money);
-
-// период(в месяцах) накопления цели
-let getTargetMonth = target => {
-    return ( Math.ceil(target / accumulatedMonth) );
-};
-
-let targetMonth = getTargetMonth(mission);
-
-// вывод срока выполнения цели
-let showTargetMonth = function() {
-    if ( targetMonth > 0 ) {
-        console.log(`Цель будет достигнута за: ${targetMonth} месяцев`);
-    } else {
-        console.log('Цель не будет достигнута');
-    }
-};
-
-// типы данных
-let showTypeOf = data => {
-    return ( console.log(typeof(data)) );
-};
-
-showTypeOf(money);
-showTypeOf(income);
-showTypeOf(deposit);
-
-console.log(`Месячные расходы равны: ${expensesAmount} рублей`);
-
-console.log(`Возможные расходы в месяц: ${addExpenses.split(',')}`);
-
-showTargetMonth();
-
-let budgetDay = Math.floor(accumulatedMonth / 30);
-console.log(`дневной бюджет: ${budgetDay} рублей`);
-
-// получение статуса с учетом заработка и расходов
-let getStatusIncome = (budget) => {
-    if (budget > 1200) {
-        return ('У вас высокий уровень дохода!');
-    } else if ( (budget <= 1200) && (budget > 600) ) {
-        return ('У вас средний уровень дохода');
-    } else if (budget <= 600 && budget >= 0) {
-        return ('К сожалению у вас уровень дохода ниже среднего');
-    } else {
-        return ('Что то пошло не так');
+        appData.expensesMonth = sum;
+    },
+    // месячный и дневной бюджет с учетом обязательных расходов
+    getBudget: function() {
+        appData.budgetMonth = appData.budget - appData.expensesMonth;
+        
+        appData.budgetDay = Math.floor(appData.budgetMonth / 30);
+    },
+    // период(в месяцах) накопления цели
+    getTargetMonth: function() {
+        return ( Math.ceil(appData.mission / appData.budgetMonth) );
+    },
+    // вывод срока выполнения цели
+    showTargetMonth: function() {
+        if ( appData.getTargetMonth() > 0 ) {
+            console.log(`Цель будет достигнута за: ${appData.getTargetMonth()} месяцев`);
+        } else {
+            console.log('Цель не будет достигнута');
+        }
+    },
+    // получение статуса с учетом заработка и расходов
+    getStatusIncome: function() {
+        if (appData.budgetDay > 1200) {
+            return ('У вас высокий уровень дохода!');
+        } else if ( (appData.budgetDay <= 1200) && (appData.budgetDay > 600) ) {
+            return ('У вас средний уровень дохода');
+        } else if (appData.budgetDay <= 600 && appData.budgetDay >= 0) {
+            return ('К сожалению у вас уровень дохода ниже среднего');
+        } else {
+            return ('Что то пошло не так');
+        }
     }
 };
 
-console.log(getStatusIncome(budgetDay));
+appData.asking();
+appData.getExpensesMonth();
+appData.getBudget();
+
+console.log(`Месячные расходы равны: ${appData.expensesMonth} рублей`);
+
+appData.showTargetMonth();
+
+console.log(appData.getStatusIncome());
+
+console.log('Наша программа включает в себя данные: ');
+for (let key in appData) {
+    console.log(`ключ:${key} is ${appData[key]}`);
+}
 
  
