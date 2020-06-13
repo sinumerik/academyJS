d6e32ee55c5c8918d8b005f5db5c3b114cb6bea9
sinumerik <2603049@gmail.com>
2020-06-13 13:41:56 +0300

    rebuild lesson, output in HTML

--- main.js
+++ main.js
@@ -1,27 +1,56 @@
 'use strict';
 
-let week = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
+const week = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье']; 
 
-let date = new Date();
+const block = document.getElementById('block');
 
-function getDayOfWeek() {
-    return date.getDay();
-}
+const date = new Date();
 
-week.forEach(function(item, index, arr) {
-    if ( (index === arr.length - 1) || (index === arr.length - 2) ) {
-        if (index === getDayOfWeek()) {
-            console.log('%c' + item, 'color: red; font-style: italic');
-        } else {
-            console.log('%c' + item, 'color: red');
-        }
-    } else {
-        if (index === getDayOfWeek()) {
-            console.log('%c' + item, 'font-style: italic');
-        } else {
-            console.log(item);
-        }
-        
+week.forEach(function(item, i, arr) {
+    let day = document.createElement('span');
+
+    day.innerText = item;
+
+    if (i === arr.length - 1 || i === arr.length - 2) {
+        day.classList.add('weekend');
     }
+
+    if (date.getDay() - 1 === i) {
+        day.classList.add('current');
+    }
+
+    block.append(day);
 });
+
+
+
+
+
+
+
+
+
+
+// let date = new Date();
+
+// function getDayOfWeek() {
+//     return date.getDay();
+// }
+
+// week.forEach(function(item, index, arr) {
+//     if ( (index === arr.length - 1) || (index === arr.length - 2) ) {
+//         if (index === getDayOfWeek()) {
+//             console.log('%c' + item, 'color: red; font-style: italic');
+//         } else {
+//             console.log('%c' + item, 'color: red');
+//         }
+//     } else {
+//         if (index === getDayOfWeek()) {
+//             console.log('%c' + item, 'font-style: italic');
+//         } else {
+//             console.log(item);
+//         }
+        
+//     }
+// });
 
