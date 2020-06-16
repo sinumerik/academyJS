fb7971718dd645859412608c5c81aa089383b73e
sinumerik <2603049@gmail.com>
2020-06-16 13:46:26 +0300

    added comments

--- main.js
+++ main.js
@@ -2,29 +2,29 @@
 
 let // кнопка запуска программы
     start = document.getElementById('start'),
-    //
+    // кнопка добавления возможного дохода
     addIncome = document.getElementsByTagName('button')[0],
-    //
+    // кнопка добавления возможных расходов
     addExpenses = document.getElementsByTagName('button')[1],
-    //
+    // чекбокс о наличии депозита
     depositCheck = document.querySelector('#deposit-check'),
-    //
+    // возможные доходы
     additionalIncomeItems = document.querySelectorAll('.additional_income-item'),
-    //
+    // поля с выводом значений
     values = document.querySelectorAll('[class$="-value"]'),
-    //
-    salary = document.querySelector('.salary-amount'),
-    //
+    // месячный доход
+    salaryAmount = document.querySelector('.salary-amount'),
+    // название дополнительного дохода
     incomeTitle = document.querySelector('.income-title'),
-    //
+    // значение дополнительного дохода
     incomeAmount = document.querySelector('.income-amount'),
-    //
+    // название обязательных расходов
     expensesTitle = document.querySelector('.expenses-title'),
-    //
+    // значение обязательных расходов
     expensesAmount = document.querySelector('.expenses-amount'),
-    //
+    // возможные расходы
     additionalExpensesItem = document.querySelector('.additional_expenses-item'),
-    //
+    // цель 
     targetAmount = document.querySelector('.target-amount'),
-    //
+    // период рассчета
     periodSelect = document.querySelector('.period-select');
