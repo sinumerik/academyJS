701338a2c2bceaa8eb9307249c27cfdce30c148d
sinumerik <2603049@gmail.com>
2020-06-23 11:38:02 +0300

    hardLesson09

---
+++ main.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const colorValue = document.querySelector('.color-value'),
+    colorPicker = document.querySelector('.color-picker'),
+    body = document.querySelector('body');
+
+function randomColor() {
+
+    let str = '0123456789abcdef';
+    let maxPosition = str.length - 1;
+    let result = '';
+
+    for (let i = 0; i < 6; i++) {
+        result += str[Math.floor( Math.random() * maxPosition )];
+    }
+
+    return '#' + result;
+}
+
+colorPicker.addEventListener('click', function() {
+    body.style.background = randomColor();
+
+    colorValue.textContent = randomColor().toUpperCase();
+})
